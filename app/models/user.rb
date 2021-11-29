fb72c0742bbaddd4162fe3791b7eb5e55169163e
Braulio <b.oyarzun.barros@gmail.com>
2021-11-29 19:20:03 -0300

    final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
+  PROFILE_PICTURE_BY_DEFAULT = "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+
   has_many :tweets
   has_many :likes, dependent: :destroy
+
+  before_save :set_default_picture
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def set_default_picture
+    if self.profile_picture.empty?
+      self.profile_picture = PROFILE_PICTURE_BY_DEFAULT
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_11_27_231133) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "likes", force: :cascade do |t|
-    t.integer "tweet_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tweet_id"], name: "index_likes_on_tweet_id"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_11_27_231133) do
   create_table "tweets", force: :cascade do |t|
     t.string "content", null: false
     t.integer "retweet_id"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tweets_on_user_id"
