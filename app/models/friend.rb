7e8b69a16d14680c9e70b141cc78144004fda8b3
Braulio <b.oyarzun.barros@gmail.com>
2021-12-04 02:08:38 -0300

    following button with tailwind

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,7 +1,4 @@
 class Friend < ApplicationRecord
   belongs_to :follower_user, foreign_key: 'user_id', class_name: 'User'
   belongs_to :followed_user, foreign_key: 'friend_id', class_name: 'User'
-
-  validates :friend_id, uniqueness: true
-  validates :user_id, uniqueness: true
 end
