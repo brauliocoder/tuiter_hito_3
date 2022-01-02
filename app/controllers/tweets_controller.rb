e09d100f69553ef769f8776f8336ee651059c887
Braulio <b.oyarzun.barros@gmail.com>
2022-01-02 17:21:33 -0300

    fixed search bugs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,10 +9,9 @@ class TweetsController < ApplicationController
   # GET /tweets or /tweets.json
   def index
     @page = params.fetch(:page, 0).to_i
-    @pages = (Tweet.all.count / TWEETS_PER_PAGE.to_f).ceil
+    @pages = (Tweet.search(params[:query]).count / TWEETS_PER_PAGE.to_f).ceil
     
-    # @tweets = Tweet.offset(@page * TWEETS_PER_PAGE).limit(TWEETS_PER_PAGE)
-    @tweets = Tweet.search(params[:query])
+    @tweets = Tweet.search(params[:query]).offset(@page * TWEETS_PER_PAGE).limit(TWEETS_PER_PAGE)
   end
 
   # GET /tweets/1 or /tweets/1.json
